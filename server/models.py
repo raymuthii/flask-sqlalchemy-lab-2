d30e6444ecce08afb3c9eed96e3b1c5d60f34fd1
Raymond Ruibi Muthii <raymondmuthii@gmail.com>
2024-10-25 13:03:52 +0300

    </3

--- server/models.py
+++ server/models.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, ForeignKey
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
-
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,22 +10,51 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    # Relationship to reviews
+    reviews = db.relationship('Review', back_populates='customer')
+    # Association proxy to items through reviews
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationship to reviews
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # Foreign keys
+    customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, ForeignKey('items.id'))
+
+    # Relationships
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
